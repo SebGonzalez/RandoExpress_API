35831e3bce0e6671af64e6791f33a50361761d46
Sébastien Gonzalez <>
2020-01-25 17:20:29 +0100

    Création complète des entités

--- src/models/Personne.java
+++ src/models/Personne.java
@@ -1,10 +1,15 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Personne {
@@ -12,17 +17,38 @@ public class Personne {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	@Column
+	@Basic(optional = false)
+	@Column(nullable = false)
 	private String name;
+	@Basic(optional = false)
+	@Column(nullable = false)
+	private String firstName;
+	@Basic(optional = false)
+	@Column(nullable = false, unique=true)
+	private String uuid;
+	@ManyToMany
+	private List<Rando> randos;
 	
 	public Personne() {
 		
 	}
-	
-	public Personne(String name) {
+
+	public Personne(String name, String firstName, String uuid) {
+		super();
 		this.name = name;
+		this.firstName = firstName;
+		this.uuid = uuid;
+		this.randos = new ArrayList<Rando>();
 	}
 	
+	public Personne(String name, String firstName, String uuid, List<Rando> randos) {
+		super();
+		this.name = name;
+		this.firstName = firstName;
+		this.uuid = uuid;
+		this.randos = randos;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -35,6 +61,31 @@ public class Personne {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	
+	public List<Rando> getRandos() {
+		return randos;
+	}
+
+	public void setRandos(List<Rando> randos) {
+		this.randos = randos;
+	}
+	
 	
 	
 }
--- src/models/Rando.java
+++ src/models/Rando.java
@@ -1,5 +1,143 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Rando {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Basic(optional = false)
+	@Column(nullable = false)
+	private String name;
+
+	@Basic(optional = false)
+	@Column(nullable = false)
+	private String description;
+
+	@Basic(optional = false)
+	@Column(nullable = false)
+	private String ville;
+
+	@Basic(optional = false)
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date dateDepart;
+
+	@Basic(optional = false)
+	@Column(nullable = false)
+	private String longitude;
+
+	@Basic(optional = false)
+	@Column(nullable = false)
+	private String latitude;
+
+	@ManyToMany(mappedBy = "randos")
+	private List<Personne> persons;
+
+	public Rando() {
+
+	}
+
+	public Rando(String name, String description, String ville, Date dateDepart, String longitude, String latitude) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.ville = ville;
+		this.dateDepart = dateDepart;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.persons = new ArrayList<Personne>();
+	}
+
+	public Rando(String name, String description, String ville, Date dateDepart, String longitude, String latitude,
+			List<Personne> persons) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.ville = ville;
+		this.dateDepart = dateDepart;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.persons = persons;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public Date getDateDepart() {
+		return dateDepart;
+	}
+
+	public void setDateDepart(Date dateDepart) {
+		this.dateDepart = dateDepart;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public List<Personne> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(List<Personne> persons) {
+		this.persons = persons;
+	}
 
 }
