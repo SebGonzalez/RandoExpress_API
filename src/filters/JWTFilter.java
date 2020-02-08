7b7431b6475c575e800107bf5bdcb0edd47eedd4
Sébastien Gonzalez <>
2020-02-09 00:16:42 +0100

    Ajout authentification (mail/mdp) + filtre du jwt à chaque requête

---
+++ src/filters/JWTFilter.java
@@ -0,0 +1,60 @@
+package filters;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+
+@Provider
+@PreMatching
+@WithMyFilter
+public class JWTFilter implements ContainerRequestFilter {
+
+	@Override
+	public void filter(ContainerRequestContext ct) throws IOException {
+
+		System.out.println(ct.getUriInfo().getPath());
+		if(ct.getUriInfo().getPath().contains("auth")) return;
+		
+		boolean ok = false;
+		Map<String, List<String>> headrs = ct.getHeaders();
+
+		// String jwt = headrs.get("Authorization");
+		System.out.printf("-- PreMatching \n");
+		for (Map.Entry<String, List<String>> entry : headrs.entrySet()) {
+			
+			if (entry.getKey().equals("Authorization")) {
+				ok = true;
+
+				String token = entry.getValue().get(0);
+				System.out.println("TOOOOKEN " + token);
+				try {
+					Algorithm algorithm = Algorithm.HMAC256("secret");
+					JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth0").build(); // Reusable verifier
+																								// instance
+					DecodedJWT jwt = verifier.verify(token);
+				} catch (JWTVerificationException exception) {
+					System.out.println("BAD request : " + exception.getMessage());
+					ct.abortWith(Response.status(Response.Status.BAD_REQUEST).build());
+				}
+				break;
+			}
+
+		}
+		if(!ok)
+			ct.abortWith(Response.status(Response.Status.BAD_REQUEST).build());
+
+	}
+
+}
