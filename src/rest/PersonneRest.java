e339f93a57889af50a3be097856609597005276b
Chaabane <mohamed.chaabane@etu.univ-amu.fr>
2020-02-20 16:50:08 +0100

    changement pom pour maven

--- src/rest/PersonneRest.java
+++ src/rest/PersonneRest.java
@@ -16,7 +16,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import dao.PersonneDAO;
-import filters.WithMyFilter;
+//import filters.WithMyFilter;
 import models.Personne;
 
 @Path("/rest")
